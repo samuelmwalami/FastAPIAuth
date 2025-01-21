812844afd2d47810d6d939ce9219ad743613d696
samuel mwalami <samuelmwalami@gmail.com>
2025-01-21 17:30:25 +0300

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
+
+oauth_2 = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
 @app.get("/home")
-async def home() -> dict:
-    pass
+async def home(token: Annotated[str, Depends(oauth_2)]) -> dict:
+    return {"message": "SUCCESSFUL",
+            "token" : token,}
