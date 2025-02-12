f4d984d9156bdbdd534cdca13e5dc785f75d6496
samuel mwalami <samuelmwalami@gmail.com>
2025-02-12 12:24:20 +0300

    Implemented password hashing

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from typing import Annotated
+from pydantic import BaseModel
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
 
 oauth_2 = OAuth2PasswordBearer(tokenUrl="token")
 
+class User(BaseModel):
+    name: str
+    password: str
+    age: int
+
+fake_db = [
+    {"name" : "John Doe",
+     "password" : "password",
+     "age" : 20,},
+    {"name" : "Jane Doe",
+     "password" : "password",
+     "age" : 20,},
+]
+
+async def decode_token(token):
+    pass
+    
+async def get_user(token: Annotated[str, Depends(oauth_2)]):
+    pass
+
 app = FastAPI()
 
 @app.get("/home")
-async def home(token: Annotated[str, Depends(oauth_2)]) -> dict:
+async def home(token: Annotated[str, Depends(get_user)]) -> dict:
     return {"message": "SUCCESSFUL",
             "token" : token,}
